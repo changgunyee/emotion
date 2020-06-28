108f2d738c071e2db9b2baea7a3aa2d565cb7ffd
changgunyee <minyoung403@naver.com>
2020-06-28 13:40:59 +0900

    add tutorial cnn

---
+++ test.py
@@ -0,0 +1,23 @@
+import pytest
+from preprocessor import *
+
+utters_train=["Oh my God, he\u0092s lost it. He\u0092s totally lost it.",
+              "What?",
+              "Or! Or, we could go to the bank, close our accounts and cut them off at the source."]
+labels_train=["non-neutral","surprise","neutral"]
+utters_test=["Why do all you\u0092re coffee mugs have numbers on the bottom?",
+            "Oh. That\u0092s so Monica can keep track. That way if one on them is missing, she can be like, \u0091Where\u0092s number 27?!\u0092"]
+labels_test=[ "surprise","non-neutral"]
+utters = utters_train + utters_test
+
+
+def test_cal_max_length():
+    max_seq_word_length = cal_max_length(utters)
+    assert max_seq_word_length==22
+
+def test_preorcess():
+    tokenizer = model_tokenizer(utters)
+    vocab_size = len(tokenizer.word_index) + 1
+    x_train, y_train, x_test, y_test, max_seq_len = preprocess(utters_train, labels_train, utters_test, labels_test,
+                                                               tokenizer)
+    print(x_train,y_train,x_test,y_test,max_seq_len)
---
+++ tutorial.py
@@ -0,0 +1,103 @@
+from constants import *
+from utils import *
+import numpy as np
+from keras.models import Sequential
+from keras import layers
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Model
+
+def create_embedding_matrix(filepath, word_index, embedding_dim):
+    vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+    with open(filepath,'r',encoding='utf8') as f:
+        for line in f:
+            word, *vector = line.split()
+            if word in word_index:
+                idx = word_index[word]
+                embedding_matrix[idx] = np.array(
+                    vector, dtype=np.float32)[:embedding_dim]
+
+    return embedding_matrix
+
+utters_train,labels_train=load_dialogues(DATA_MAP['train'])
+utters_dev,labels_dev=load_dialogues(DATA_MAP['dev'])
+utters_test,labels_test=load_dialogues(DATA_MAP['test'])
+utters_train+=utters_dev
+labels_train+=labels_dev
+
+utters=utters_train+utters_test
+max_word_length= max([len(utter.split()) for utter in utters])
+max_seq_length=max([len(utter) for utter in utters])
+tokenizer=Tokenizer(num_words=10000)
+tokenizer.fit_on_texts(utters)
+vocab_size = len(tokenizer.word_index) + 1
+
+x_train=tokenizer.texts_to_sequences(utters_train)
+x_test=tokenizer.texts_to_sequences(utters_test)
+
+x_train = pad_sequences(x_train, padding='post', maxlen=max_seq_length)
+x_test= pad_sequences(x_test, padding='post', maxlen=max_seq_length)
+
+
+y_train=map_label_to_idx(np.array(labels_train))
+y_test=map_label_to_idx(np.array(labels_test))
+
+
+input_dim=x_train.shape[1]
+model=Sequential()
+embedding_dim = 300
+embedding_matrix = create_embedding_matrix(
+    './glove.42B.300d.txt',
+    tokenizer.word_index, embedding_dim)
+
+seq_input = layers.Input(shape=(max_seq_length,), dtype='int32')
+seq_embedded=layers.Embedding(input_dim=vocab_size,
+                           output_dim=embedding_dim,
+                           weights=[embedding_matrix],
+                           input_length=max_seq_length,
+                           trainable=True)(seq_input)
+
+
+filters=[2,3,4,5]
+conv_models=[]
+for filter in filters:
+    conv_feat = layers.Conv1D(filters=128,
+                            kernel_size=filter,
+                            activation='relu',
+                            padding='valid')(seq_embedded) #Convolution Layer
+    pooled_feat = layers.GlobalMaxPooling1D()(conv_feat) #MaxPooling
+    conv_models.append(pooled_feat)
+
+conv_merged = layers.concatenate(conv_models, axis=1) #filter size가 2,3,4,5인 결과들 Concatenation
+model_output = layers.Dropout(0.2)(conv_merged)
+# model_output = layers.Dense(10, activation='relu')(model_output)
+# logits = layers.Dense(1, activation='sigmoid')(model_output)
+model_output=layers.Dense(10,input_dim=input_dim,activation='relu')(model_output)
+logits=layers.Dense(8,activation='softmax')(model_output)
+model = Model(seq_input, logits) #(입력,출력)
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+history=model.fit(x_train,y_train,
+                  epochs=10,
+                  validation_data=(x_test,y_test),
+                  batch_size=128)
+plot_history(history)
+model.save('my_model.h5')
+
+loss, accuracy = model.evaluate(x_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+
+loss, accuracy = model.evaluate(x_test, y_test, verbose=False)
+print("Test Accuracy: {:.4f}".format(accuracy))
+
+
+x_submission=load_csv_dialogues(DATA_MAP['submission_input'])
+x_submission=tokenizer.texts_to_sequences(x_submission)
+x_submission= pad_sequences(x_submission, padding='post', maxlen=max_seq_length)
+
+predictions=model.predict(x_submission)
+classe_idxs=np.argmax(predictions,axis=1)
+y_submission=map_idx_to_label(classe_idxs)
+output_to_csv(DATA_MAP['submission_output'],y_submission)
